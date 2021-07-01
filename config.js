5bf2e2228995b63f088a48e3a17854818b7f01cd
honghanh03 <86757043+honghanh03@users.noreply.github.com>
2021-07-01 22:43:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Babe',
+    introDesc: `Anh iu em đúng hong :)) `,
+    btnIntro: 'Bấm vào đây trả lời nè ',
+    title: 'Ngoài em ra không ai tấu hài anh xem đâu :)) ',
+    desc: 'Nên chịu khó iu em đi :)) ',
+    btnYes: 'Yêu bé :3',
+    btnNo: 'không thử coi?',
+    question:'Yêu em dzui khum? =))',
+    btnReply: 'Gửi bé',
+    reply: 'Yêu thì yêu mà không yêu thì ráng mà yêu :))',
+    mess: 'Em biết anh yêu em mà =)).',
+    messDesc: 'Bin hứa thi xong dẫn em đi chơi nhó',
+    btnAccept: 'Okiiiii',
+    messLink: 'http://fb.com'//https://m.me/baybyhonghanh
 }
