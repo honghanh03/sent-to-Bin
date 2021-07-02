e52719e8be6634a1ba8058b10d2a9e1769666f57
honghanh03 <86757043+honghanh03@users.noreply.github.com>
2021-07-02 09:41:41 +0700

    Update config.js

--- https:/github.com/namndwebdev/tang-crush/blob/main/config.js
+++ https:/github.com/namndwebdev/tang-crush/blob/main/config.js
@@ -1,1 +1,16 @@
-
+const CONFIG = {
+    introTitle: 'Hi bae',
+    introDesc: `Anh iu bé đúng khum? :))`,
+    btnIntro: 'Bấm vào đây để trả lời nè :))',
+    title: 'PNgoài em ra không ai tấu hài anh coi đâu',
+    desc: 'Nên anh chịu khó iu em đi ',
+    btnYes: 'Anh iu bé',
+    btnNo: 'Không thử coi? :)',
+    question:'Sao Bin iu em?',
+    btnReply: 'Send me',
+    reply: 'Yêu thì yêu mà không yêu thì ráng mà iu',
+    mess: 'Em biết anh iu em mà :)',
+    messDesc: 'Bin hứa thi xong dẫn em đi chơi rồi nhó',
+    btnAccept: 'Okiiii',
+    messLink: 'http://fb.com'https://www.facebook.com/
+}
